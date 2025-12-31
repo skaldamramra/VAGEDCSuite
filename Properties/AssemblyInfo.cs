769306e214986095e14fc38e852755ec950e6ebc
skaldamramra <skaldamramra@yahoo.com>
2025-12-31 21:39:38 +0100

    chore(release): bump version to 1.4.0.9 and update copyright year

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Dilemma")]
 [assembly: AssemblyProduct("VAGSuite")]
-[assembly: AssemblyCopyright("Copyright © Dilemma 2013, Mod by Skalda 2023")]
+[assembly: AssemblyCopyright("Copyright © Dilemma 2013, Mod by Skalda 2025")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.4.0.8")]
-[assembly: AssemblyFileVersion("1.4.0.8")]
+[assembly: AssemblyVersion("1.4.0.9")]
+[assembly: AssemblyFileVersion("1.4.0.9")]
